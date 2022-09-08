55b25bf0b0e22f18e86cf7820547e870549f67bc
Sohvi Puska <sohvip1@gmail.com>
2022-09-08 18:26:25 +0300

    osa1

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -41,7 +41,6 @@ const Statistics = ({counterg, countern, counterb, countera, counterav, counterp
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
