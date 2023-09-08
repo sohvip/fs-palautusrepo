a560235929eb056618c2141e78f085b9573a2441
Sohvi Puska <sohvip1@gmail.com>
2023-09-08 11:05:05 +0300

    2e

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Filter = ({ value, onChange }) => 
   <div>
-    find countries<input value={value} onChange={onChange}/>
+    find countries <input value={value} onChange={onChange}/>
   </div>
 
 const Countries = ({ countries, filter, showcountry, handleshow }) => {
@@ -50,9 +50,9 @@ const Country = ({ country, handleshow }) => {
 }
 
 const OneCountry = ({ country }) => {
-  const [weather, setWeather] = useState(undefined)
-  const api_key = process.env.REACT_APP_API_KEY
-  const address = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`
+  const [weather, setWeather] = useState(null)
+  const apiKey = process.env.REACT_APP_API_KEY
+  const address = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${apiKey}&units=metric`
   useEffect(() => {
     axios
       .get(address)
@@ -60,24 +60,28 @@ const OneCountry = ({ country }) => {
         setWeather(response.data)
       })
   }, [])
-  return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>capital {country.capital}</p>
-      <p>area {country.area}</p>
-      <h3>languages:</h3>
-      <ul>
-        {Object.values(country.languages).map(language =>
-          <li key={language}>{language}</li>
-        )}
-      </ul>
-      <img src={country.flags.png} />
-      <h2>Weather in {country.capital}</h2>
-      <p>temperature {weather?.main.temp} celsius</p>
-      <img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} />
-      <p>wind {weather?.wind.speed} m/s </p>
-    </div>
-  )
+  if (weather === null) {
+    return null
+  } else {
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>capital {country.capital}</p>
+        <p>area {country.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map(language =>
+            <li key={language}>{language}</li>
+          )}
+        </ul>
+        <img src={country.flags.png} />
+        <h2>Weather in {country.capital}</h2>
+        <p>temperature {weather?.main.temp} celsius</p>
+        <img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} />
+        <p>wind {weather?.wind.speed} m/s </p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,13 +4,27 @@ import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import personService from './services/persons'
+import './index.css'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='popup'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
-
+  const [popupMessage, setPopupMessage] = useState(null)
+  
   useEffect(() => {
     personService
       .getAll()
@@ -40,7 +54,11 @@ const App = () => {
         .then(response =>
           setPersons(persons.concat(response))
         )
-      }
+      setPopupMessage(`Added ${personObject.name}`)
+      setTimeout(() => {
+        setPopupMessage(null)
+      }, 4000)
+    }
     setNewName('')
     setNewNumber('')
   }
@@ -50,7 +68,11 @@ const App = () => {
       personService
         .remove(person.id)
       setPersons(persons.filter(n => n.id !== person.id))
-        }
+      setPopupMessage(`Deleted ${person.name}`)
+      setTimeout(() => {
+        setPopupMessage(null)
+      }, 4000)
+      }
     }
 
   const handleNumberMod = (person) => {
@@ -59,7 +81,11 @@ const App = () => {
       .update(person.id, changedNumber)
       .then(response => {
       setPersons(persons.map(p => p.id !== person.id ? p : response))
-    })
+      })
+    setPopupMessage(`Number of ${person.name} is now changed`)
+    setTimeout(() => {
+      setPopupMessage(null)
+    }, 4000)
   }
 
   const checkDuplicate = () => {
@@ -91,6 +117,7 @@ const App = () => {
       <form>
         <Filter value={newFilter} change={handleFilterChange} />
         <h2>Add a new person</h2>
+        <Notification message={popupMessage} />
         <PersonForm value={newName} change={handleNameChange} value2={newNumber} change2={handleNumberChange} click={handleAdd} />
       </form>
       <h2>Numbers</h2>
